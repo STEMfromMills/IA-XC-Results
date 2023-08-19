57f36c48412c3b2507530e49dd403175e42468fc
stemfrommills <jmills@nevadacubs.org>
2023-08-19 09:10:58 -0500

    class 1A cleaned up and merged

--- find_and_replace_in_all_csv.py
+++ find_and_replace_in_all_csv.py
@@ -30,7 +30,7 @@ def find_and_replace_in_csv(folder_path, search_term, replace_term):
 
 if __name__ == "__main__":
     folder_path = "csv input"
-    search_term = "1A MARTENSDALE LEVI WEBB 2025"
-    replace_term = "1A MARTENSDALE LEVI WEBB 2023"
+    search_term = "1A WOODWARD GRANGER AIDEN TOPALOVICH 2024"
+    replace_term = "1A WOODWARD GRANGER AIDEN TOPOLAVICH 2024"
 
     find_and_replace_in_csv(folder_path, search_term, replace_term)
